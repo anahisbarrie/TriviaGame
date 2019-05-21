d07770c63ec913990f3264c81e5d9e3b5d8c3a28
anahisbarrie <anahis.barrie@gmail.com>
2019-05-21 13:31:05 -0400

    finished project

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,65 @@
 //initialize global variables
-var Start = 0 
-var unAnswer = 0
+var unAnswer = 5
 var Answer = " "
 var CorrectAnswers = 0
 var IncorrectAnswers = 0
 var CorrectArr = ["Venezuela", "19,354", "Havanesse", "middle", "No-one"]
-var Done = 0 
-var timer = 0 
+var Timer = 0 
 
 // Function to start the game 
 $("#Start").click(function(){
+    start()
     var timeleft = 15;
-    var downloadTimer = setInterval(function () {
+    Timer = setInterval(function () {
         document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
         timeleft -= 1;
         if (timeleft <= 0) {
-            clearInterval(downloadTimer);
-            document.getElementById("timer").innerHTML = "Time is up!"
-            // FinishGame()
+            done()  
         }
     }, 1000);
 })
 
 $('.custom-control-input').click(function(event) {
     var answer = event.target.value
-    
     console.log(event.target.value)
-
     for (var i = 0; i < CorrectArr.length; i++) {
         if (answer === CorrectArr[i]) {
             CorrectAnswers +=1;
             return 
         }
-        
-        // else if (answer === 0) {
-        //     unAnswer = + 1;
-        // }
+      
     }
-   
-            IncorrectAnswers +=1;
+            IncorrectAnswers +=1;     
 } );
 
+function start(){
 
-// var obj = {
-//    CorrectAnswers = 0,
-//     IncorrectAnswers = 0,
-//     num = 0,
-//     unAnswer = 0 
-// }
+    unAnswer = 5
+    Answer = " "
+    CorrectAnswers = 0
+    IncorrectAnswers = 0
+    CorrectArr = ["Venezuela", "19,354", "Havanesse", "middle", "No-one"]
+    Timer = 0 
+    document.getElementById("corrects").innerHTML = "Correct Answers: " + CorrectAnswers;
+    document.getElementById("Incorrects").innerHTML = "Incorrect Answers: " + IncorrectAnswers;
+    document.getElementById("notanswered").innerHTML = "Not Answered: 0";
+    $('.custom-control-input').prop( "checked", false )
+    $("#my-questions").css("display", "block")
+}
 
-//  function FinishedGame(){
-//      obj.num !== 0     
-//      var unAnswer = 0
-//      var Answer 
-//      var CorrectAnswers 
-//      var IncorrectAnswers 
-//  }
+$("#done").click(done)
 
+function done () {
+    unAnswer = unAnswer - CorrectAnswers - IncorrectAnswers
 
-// // Function to end the game 
-
-var Done = $("#done").click(function (){
-
-    // clearInterval(downloadTimer);
-
+    clearInterval(Timer);
+    
+    document.getElementById("timer").innerHTML = "Well Done! Check your Results Below!"
     document.getElementById("corrects").innerHTML = "Correct Answers: " + CorrectAnswers;
     document.getElementById("Incorrects").innerHTML = "Incorrect Answers: " + IncorrectAnswers;
     document.getElementById("notanswered").innerHTML = "Not Answered: " + unAnswer;
+    $("#my-questions").css("display", "none")
 
-
-});
+}
 
 
